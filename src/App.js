78d019f5b26d8927b3ae41ff5dcfd69a9212eaa2
Animesh-03 <adithaanimesh76@gmail.com>
2022-04-15 12:35:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [inputCode,setInputCode] = useState([]);
+  const [inputParams,setInputParams] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='question-area'>
+        Lorem Ipsum Dolor Sit Amet
+        Lorem Ipsum Dolor Sit Amet
+        Lorem Ipsum Dolor Sit Amet
+        Lorem Ipsum Dolor Sit Amet
+        Lorem Ipsum Dolor Sit Amet
+        Lorem Ipsum Dolor Sit Amet
+      </div>
+      <div className='code-area'>
+          <div className='input-area'>
+              <textarea className='form-control' rows={25} placeholder='Enter C++ Code here' onChange={e => setInputCode(e.target.value)} />
+              <br />
+              <textarea className='form-control' rows={2} placeholder='Enter Input Parameters here' onChange={e => setInputParams(e.target.value)} />
+              <br />  
+              <button className='btn btn-primary'>Compile</button>
+          </div>
+
+          <div className='output-area'>
+              Output here
+          </div>
+      </div>
     </div>
   );
 }
